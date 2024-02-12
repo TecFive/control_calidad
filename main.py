e01b9f9e49e62467c0e2e16ebbdae39af52f1f26
J. Lugo <a01571142@tec.mx>
2024-02-12 14:13:06 -0600

    solved

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import numpy as np
 import cv2
 import os
 
-def compare(i1,i2):
-    diferencia = cv2.subtract(i1,i2) 
-    
+def compare(i1, i2):
+    i2_resized = cv2.resize(i2, (i1.shape[1], i1.shape[0]))
+    diferencia = cv2.subtract(i1, i2_resized)
     if not np.any(diferencia):
-        print("Good quality") 
+        print("Perfect")
     else:
-        print("Bad quality")
+        # Calcula el porcentaje de similitud
+        porcentaje_similitud = (np.count_nonzero(diferencia == 0) / diferencia.size) * 100
+        print(f"Imperfect - Similarity: {porcentaje_similitud:.2f}%")
 
 sample = cv2.imread("Assets/original.png")
 
-for i in range(1,13):
+for i in range(1, 13):
     tmpimg = cv2.imread(f"Assets/{i}.png")
-    compare(sample,tmpimg)
+    compare(sample, tmpimg)
