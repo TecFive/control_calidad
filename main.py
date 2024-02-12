e82eab4740a26a31257457be4ff40e1d36f86b79
J. Lugo <a01571142@tec.mx>
2024-02-12 13:59:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import numpy as np
 import cv2
 import os
 
-sample = cv2.imread("original.png")
+def compare(i1,i2):
+    diferencia = cv2.subtract(i1,i2) 
+    
+    if not np.any(diferencia):
+        print("Good quality") 
+    else:
+        print("Bad quality")
+
+sample = cv2.imread("Assets/original.png")
 
 for i in range(1,13):
-    tmpimg = cv2.imread("{i}.png")
-    
+    tmpimg = cv2.imread(f"Assets/{i}.png")
+    compare(sample,tmpimg)
